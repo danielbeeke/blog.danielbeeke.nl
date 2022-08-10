04231221662cd9310176ad8f46b446d3a2557658
Daniel Beeke <mail@danielbeeke.nl>
2022-08-10 20:19:19 +0200

    clean up

--- build/js/App.js
+++
@@ -1,6 +0,0 @@
-if (location.hostname === 'localhost') {
-  const s = document.createElement('script')
-  s.type = "text/javascript"
-  s.src = 'http://localhost:35729/livereload.js'
-  document.head.appendChild(s)
-}
